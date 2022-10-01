908bdb4d86dcb2502a28c4874d4cc06ae4aae86c
Татьяна Пислегина <ztv-1988@yandex.ru>
2022-10-01 18:50:34 +0500

    Test

--- src/test/java/ru/netology/WebDriverTest.java
+++ src/test/java/ru/netology/WebDriverTest.java
@@ -1,11 +1,18 @@
 package ru.netology;
-import io.github.bonigarcia.seljup.SeleniumExtension;
+////import io.github.bonigarcia.seljup.SeleniumExtension;
+////import io.github.bonigarcia.wdm.WebDriverManager;
+//import org.junit.jupiter.api.AfterEach;
+//import org.junit.jupiter.api.BeforeAll;
+//import org.junit.jupiter.api.BeforeEach;
+//import org.junit.jupiter.api.Test;
+//import org.junit.jupiter.api.extension.ExtendWith;
+//import org.openqa.selenium.By;
+//import org.openqa.selenium.*;
+//import org.openqa.selenium.chrome.ChromeDriver;
+//import org.openqa.selenium.chrome.ChromeOptions;
+
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -24,13 +31,17 @@ public class WebDriverTest {
         options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--no-sandbox");
         options.addArguments("--headless");
-        driver = new ChromeDriver(options);
+        driver = new ChromeDriver();//(options);
     }
 
+    //    @BeforeAll
+//    public static void setupClass() {
+//        System.setProperty("webdriver.chrome.driver", "./Drivers/win/chromedriver.exe");
+//        // WebDriver.chromedriver().setup();
+//    }
     @BeforeAll
-    public static void setupClass() {
-        System.setProperty("webdriver.chrome.driver", "./Drivers/win/chromedriver.exe");
-        // WebDriver.chromedriver().setup();
+    static void setUpAll() {
+        WebDriverManager.chromedriver().setup();
     }
 
     @AfterEach
@@ -42,17 +53,43 @@ public class WebDriverTest {
     @Test
     void fullFilling() {
         driver.get("http://localhost:9999");
-        driver.findElement(By.cssSelector("[data-test-id=name]input")).sendKeys("Попов Игорь");
-        driver.findElement(By.cssSelector("[data-test-id=phone]input")).sendKeys("+79825368978");
+        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Попов Игорь");
+        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79825368978");
         driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
-        driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
-        driver.findElement(By.cssSelector(".button__text")).submit();
+        // driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
+        driver.findElement(By.cssSelector(".button__text")).click();
         String expected = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
         String actual = driver.findElement(By.cssSelector("[data-test-id=order-success]")).getText().trim();
         assertEquals(expected, actual);
-
-
     }
+
+    @Test
+    void noName() {
+        driver.get("http://localhost:9999");
+        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("");
+        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79825368978");
+        driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
+        // driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
+        driver.findElement(By.cssSelector(".button__text")).click();
+        String expected = "Поле обязательно для заполнения";
+        String actual = driver.findElement(By.cssSelector(".input__sub")).getText().trim();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void engName() {
+        driver.get("http://localhost:9999");
+        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Popov Igory");
+        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79825368978");
+        driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
+        // driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
+        driver.findElement(By.cssSelector(".button__text")).click();
+        String expected = "Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.";
+        String actual = driver.findElement(By.cssSelector(".input__sub")).getText().trim();
+        assertEquals(expected, actual);
+    }
+
 }
+
 
 
